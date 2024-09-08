122c58ea7db9ca2265dbf5573f636e3f12c7403a
ParkerGits <parkerjlandon@gmail.com>
2024-09-08 14:58:56 -0700

    feat: update writing

--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -12,8 +12,7 @@ export default function PortfolioPage() {
         <div className="flex flex-col items-center justify-center space-y-2 mb-8">
           <TitleText className="mb-0">Parker&apos;s Portfolio</TitleText>
           <TypographyH4 className="mt-0 text-zinc-500 text-center text-sm sm:text-xl">
-            Seattle Pacific University, graduating June 2024 <br />
-            Smartsheet Software Engineer, starting August 2024
+            Fullstack Software Engineer at Smartsheet
           </TypographyH4>
           <a target="_blank" href="/resume.pdf" rel="noopener noreferrer">
             <AnchorText>View Resume</AnchorText>
