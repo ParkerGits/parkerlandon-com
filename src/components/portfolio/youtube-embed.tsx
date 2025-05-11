b846480c8a7b98b1a8a5d7ad878068d785f1d09a
ParkerGits <parkerjlandon@gmail.com>
2025-05-11 14:57:06 -0700

    fix video style

--- src/components/portfolio/youtube-embed.tsx
+++ src/components/portfolio/youtube-embed.tsx
@@ -1,17 +1,21 @@
 import { cn } from "@/lib/utils";
+import { CSSProperties } from "react";
 
 type YoutubeEmbedProps = {
   embedUrl: string;
   className?: string;
+  style?: CSSProperties;
 };
 
 export default function YoutubeEmbed({
   embedUrl,
   className,
+  style,
 }: YoutubeEmbedProps) {
   return (
     <iframe
       className={cn("w-full aspect-video", className)}
+      style={style}
       src={embedUrl}
       title="YouTube video player"
       frameBorder="0"
